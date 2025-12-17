dfc9b4502f80bfe7be013c56fd69628b2121b390
Marcos Yuri <marcos.yuriro@gmail.com>
2025-12-16 21:52:23 -0300

    Delete DesafioMarcos/desafio directory

--- DesafioMarcos/desafio/projeto base/sinapses/src/main/java/br/com/zaffari/sinapses/dtos/SinapseDto.java
+++
@@ -1,9 +0,0 @@
-package main.java.br.com.zaffari.sinapses.dtos;
-
-import java.time.LocalDate;
-
-public record SinapseDto(String titulo, String descricao,
-    String categoria, LocalDate data, String urlConteudo
-) {
-
-}
